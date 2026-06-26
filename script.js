6998455640b5b353a942e74931b92f5f771c8de4
leprestigebenin <leprestigevinsetspiritueux@gmail.com>
2026-06-26 15:12:57 +0200

    Add initial script.js with event listener and function

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Le Prestige est prêt !");
+});
+
+function commanderWhatsApp() {
+    window.open(
+        "https://wa.me/2290197592841?text=Bonjour%20Le%20Prestige,%20je%20souhaite%20obtenir%20des%20informations%20sur%20vos%20vins%20et%20spiritueux.",
+        "_blank"
+    );
+}
